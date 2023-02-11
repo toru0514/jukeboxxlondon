21b1d9eb1a63bbe7d99e857c87dc398b71c7c334
toru <toru.imamura0514@gmai.com>
2023-02-11 21:48:34 +0900

    タイトル追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,22 @@
+import { shopTitle } from '../data'
+import styles from './index.module.css'
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(styles)
+
 function HomePage () {
-  return <div>Welcome to Next.js</div>
+  return (
+    <div className={cx('shop-title')}>
+      {shopTitle.map((image) => (
+        <img
+          className={cx('shop-image')}
+          src={image.image}
+          alt={image.name}
+        />
+      ))}
+      <p>JUKEBOXXLONDON</p>
+    </div>
+  )
 }
 
 export default HomePage
