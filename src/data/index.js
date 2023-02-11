4208fb9fed175a4e424b2f4786689ff7f9d479b2
toru <toru.imamura0514@gmai.com>
2023-02-11 20:09:04 +0900

    タイトル追加のみ

---
+++ src/data/index.js
@@ -0,0 +1,10 @@
+const shopTitleImage = '/images/jukeboxxlondon.jpeg'
+
+export const shopTitle = [
+  {
+    id: 'jukeboxxlondon',
+    url: '',
+    image: shopTitleImage,
+    name: 'jukeboxxlondon',
+  },
+]
