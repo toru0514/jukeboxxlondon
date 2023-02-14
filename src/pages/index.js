1c4bf78ac03452e24245f4b6012fdcaba504868f
toru <toru.imamura0514@gmai.com>
2023-02-14 21:58:40 +0900

    @netlify/next追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,17 @@ const cx = classNames.bind(styles)
 
 function HomePage () {
   return (
-    <div className={cx('shop-title')}>
-      {shopTitle.map((image) => (
-        <img
-          className={cx('shop-image')}
-          src={image.image}
-          alt={image.name}
-        />
-      ))}
-      <p>JUKEBOXXLONDON</p>
+    <div className={cx('home-page')}>
+      <div className={cx('shop-title')}>
+        {shopTitle.map((image) => (
+          <img
+            className={cx('shop-image')}
+            src={image.image}
+            alt={image.name}
+          />
+        ))}
+        <p>JUKEBOXXLONDON</p>
+      </div>
     </div>
   )
 }
