e334642a77799d7c0ca9109c8a01d74c0f48abc0
toru <toru.imamura0514@gmai.com>
2023-03-14 20:19:11 +0900

    スライダー機能削除

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,11 @@
 import styles from './index.module.css'
 import classNames from 'classnames/bind'
-import Slider from '../components/Slider/Slider'
 
 const cx = classNames.bind(styles)
 
 function HomePage () {
   return (
     <div className={cx('home-page')}>
-      <Slider/>
       <p>JUKEBOXXLONDON</p>
     </div>
   )
