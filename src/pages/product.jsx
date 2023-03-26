b254564558ed2b404f4cfb1f5db08ef21211e166
toru <toru.imamura0514@gmai.com>
2023-03-26 10:02:01 +0900

    productページ説明文等追加

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -3,7 +3,7 @@ import styles from './product.module.css'
 import classNames from 'classnames/bind'
 
 const cx = classNames.bind(styles)
-const aboutusPage = () => {
+const productPage = () => {
 
   return (
     <div>
@@ -69,14 +69,69 @@ const aboutusPage = () => {
           <p className={cx('arrow-area')}>↑</p>
         </div>
       </div>
-      <p className={cx('basic-price')}>
-        BASIC STYLE ¥218,000 -tax in
-      </p>
-      <p className={cx('custom-price')}>
-        CUSTOM STYLE ¥ASK
-      </p>
+      <div className={cx('price-style')}>
+        <p className={cx('basic-price1')}>
+          BASIC STYLE
+        </p>
+        <p className={cx('basic-price2')}>
+          ¥218,000 -tax in
+        </p>
+      </div>
+      <div className={cx('price-style')}>
+        <p className={cx('custom-price1')}>
+          CUSTOM STYLE
+        </p>
+        <p className={cx('custom-price2')}>
+          ¥ASK
+        </p>
+      </div>
+      <div className={cx('jblxbm-style')}>
+        <img
+          className={cx('jblbm-image')}
+          src={'/images/jblxbm.png'}
+          alt="image"/>
+        <p>JUKEBOXX LONDON × BLACK MARKET</p>
+      </div>
+      <div className={cx('raiders-comment')}>
+        <p>ライダースジャケットにていては<br/>BLACK MARKET様とのコラボ商品になります。<br/><br/>BLACK MARKET様オリジナルのライダースジャケットをベースに使用し、カスタムさせて頂いております。
+        </p>
+        <br/>
+        <p>BLACK MARKET<br/>「オリジナルライダースジャケット」</p>
+        <br/>
+        <p>着込むほど革の中の茶がのぞいてくる所謂「茶芯レザー」を使用し、今までとは違う風合いを堪能できるようにしました。<br/>革質は、耐久性に優れ滑らかなカウレザーを使用しております。</p>
+        <img
+          className={cx('size-image')}
+          src={'/images/sizelist.png'}
+          alt="image"/>
+        <p className={cx('size-comment')}>
+          ※実際の商品と表記サイズには数cmの個体差が生じる場合がございます
+        </p>
+        <p>素材：牛革(カウハイド) <br/>生産国：パキスタン<br/>カラー：ブラック、レッド、ネイビー、パープル、ターコイズ</p>
+        <div>
+          <img
+            className={cx('color-image')}
+            src={'/images/product_image/black.png'}
+            alt="image"/>
+          <img
+            className={cx('color-image')}
+            src={'/images/product_image/red.png'}
+            alt="image"/>
+          <img
+            className={cx('color-image')}
+            src={'/images/product_image/navy.png'}
+            alt="image"/>
+          <img
+            className={cx('color-image')}
+            src={'/images/product_image/purple.png'}
+            alt="image"/>
+          <img
+            className={cx('color-image')}
+            src={'/images/product_image/turquoise.png'}
+            alt="image"/>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default aboutusPage
+export default productPage
