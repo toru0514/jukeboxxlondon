d90dbea543a9dfd66b6213d43fb6b654237dd875
toru <toru.imamura0514@gmai.com>
2023-03-26 12:13:42 +0900

    サクセスページを追加

---
+++ src/pages/success.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ContactPage = () => {
+
+  return (
+    <>
+      <div>
+        お問合せを受け付けました。ありがとうございます。
+      </div>
+    </>
+  )
+}
+
+export default ContactPage
